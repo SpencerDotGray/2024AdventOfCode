94556db9599325588043ada117001b0456f63537
GraySA17 <GraySA17@Gray.gcc.edu>
2024-12-02 00:16:44 -0500

    Day 01

---
+++ AdventOfCode/Day01/day01.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day01;
+
+public static class Day01
+{
+    public static void ExecuteOne(string data)
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach(var point in data.Split('\n').Select(e => RemoveDoubleSpace(e).Split(" ")))
+        {
+            left.Add(int.Parse(point[0]));
+            right.Add(int.Parse(point[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        int sum = 0;
+        for (int i = 0; i < left.Count; i++)
+        {
+            sum += Math.Abs(left[i] - right[i]);
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    public static void ExecuteTwo(string data)
+    {
+        var l = data.Split('\n').Select(e => RemoveDoubleSpace(e).Split(" ")).Select(e => int.Parse(e[1]));
+
+        var sum = data.Split('\n')
+            .Select(e => RemoveDoubleSpace(e).Split(" "))
+            .Select(e => int.Parse(e[0]))
+            .Aggregate(0, (t, n) => t + (n * l.Where(e => e == n).Count()));
+
+        Console.WriteLine(sum);
+    }
+
+    private static string RemoveDoubleSpace(string data)
+    {
+        if (data.IndexOf("  ") > 0)
+            return RemoveDoubleSpace(data.Replace("  ", " "));
+        return data;
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using AdventOfCode.Day01;
+
+var fileIn = File.ReadAllText(@"C:\Users\GraySA17.GRAY\Documents\DEV\2024AdventOfCode\AdventOfCode\testinput.txt");
+
+Day01.ExecuteTwo(fileIn);
